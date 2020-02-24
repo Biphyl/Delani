8f0aecf25993617d60af4dd94c826a5c8ce0a35d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-24 12:44:07 +0300

    created folder imeges and added other folders in it

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-  $(".img1").click(function(){
-    $("p1").toggle();
+  $(".click1").click(function(){
+    $(".design_icon").toggle();
+    
+
+    
+    
   });
 
 });
@@ -8,19 +12,3 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-function received(){
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("textarea").value;
-  if (name == "" || email == "" || message == ""){
-    document.getElementsByClassName("forms").innerHTML =
-    alert ("Please enter your details and a message")
-  }
-   else {
-     alert( name + " thank you for your message")
-}
