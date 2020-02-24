c7d7f2bdaef4be1301b528a5fa23479702210adc
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-24 17:14:45 +0300

    added myFunction

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ $(document).ready(function(){
   });
 })
 
+  function myFunction(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name == "" || email == "" || message == ""){
+      alert ("enter your name, email and a commment")
+    }
+     else alert( name + " ,your message is well received");
+  }
