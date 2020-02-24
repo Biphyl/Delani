f62c25f5b2b385619e5942cdc6a7450efc44e380
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-24 15:34:54 +0300

    added toggle to what we do icons

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".click1").click(function(){
+    $(".image1").toggle();
+    $(".paragraph1").toggle();
+  });
+
+  $(".click2").click(function(){
+    $(".image2").toggle();
+    $(".paragraph2").toggle();
+  });
+
+  $(".click3").click(function(){
+    $(".image3").toggle();
+    $(".paragraph3").toggle();
+  });
+})
+
+$(document).ready(function){
+  $()
+}
