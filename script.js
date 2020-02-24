7058af2724b3f0d3bc2159f98896c5982b72beb8
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-24 16:59:11 +0300

    added a function for submit

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
   $(".click1").click(function(){
-    $(".design_icon").toggle();
-    
-
-    
-    
+    $(".image1").toggle();
+    $(".paragraph1").toggle();
   });
 
-});
+  $(".click2").click(function(){
+    $(".image2").toggle();
+    $(".paragraph2").toggle();
+  });
 
+  $(".click3").click(function(){
+    $(".image3").toggle();
+    $(".paragraph3").toggle();
+  });
+})
 
-
-
+  function myFunction(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name == "" || email == "" || message == ""){
+      alert ("enter your name, email and a commment")
+    }
+     else alert( name + " ,we have received your message. Thank you for reaching out to us. ")
+  }
