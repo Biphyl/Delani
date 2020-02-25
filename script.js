16a7226e3c70ed98ecd6e75affa50c46aaed4c1f
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-25 09:31:30 +0300

    added hover effect to the portfolio images

--- script.js
+++ script.js
@@ -13,6 +13,72 @@ $(document).ready(function(){
     $(".image3").toggle();
     $(".paragraph3").toggle();
   });
+
+  $(".port1").hover(
+    function() {
+      $(".project1").show("");
+    },
+    function() {
+      $(".project1").hide("");
+    }
+  );
+  $(".port2").hover(
+    function() {
+      $(".project2").show("");
+    },
+    function() {
+      $(".project2").hide("");
+    }
+  );
+  $(".port3").hover(
+    function() {
+      $(".project3").show("");
+    },
+    function() {
+      $(".project3").hide("");
+    }
+  );
+  $(".port4").hover(
+    function() {
+      $(".project4").show("");
+    },
+    function() {
+      $(".project4").hide("");
+    }
+  );
+  $(".port5").hover(
+    function() {
+      $(".project5").show("");
+    },
+    function() {
+      $(".project5").hide("");
+    }
+  );
+  $(".port6").hover(
+    function() {
+      $(".project6").show("");
+    },
+    function() {
+      $(".project6").hide("");
+    }
+  );
+  $(".port7").hover(
+    function() {
+      $(".project7").show("");
+    },
+    function() {
+      $(".project7").hide("");
+    }
+  );
+  $(".port8").hover(
+    function() {
+      $(".project8").show("");
+    },
+    function() {
+      $(".project8").hide("");
+    }
+  );
+
 })
 
   function myFunction(){
